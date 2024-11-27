5a3c81fc7f2f2d982a597af551ac6679871a26ec
Radman <radman@dhcp-128-189-135-202.ubcsecure.wireless.ubc.ca>
2024-11-26 17:50:34 -0800

    virus object removed. sickness or health will be via human class

--- agents.py
+++ agents.py
@@ -1,20 +1,10 @@
+
 class Human:
     id_counter = 1
-    def __init__(self, x, y, npi_adherence = 0.5):
+    def __init__(self, x, y, npi_adherence = 0.5, is_infected = False):
         self.id = Human.id_counter
         Human.id_counter += 1
 
         self.x = x
         self.y = y
-
-class Virus:
-    id_counter = 1
-    def __init__(self, x, y):
-        self.id = Virus.id_counter
-        Virus.id_counter += 1
-
-        self.x = x
-        self.y = y
-        self.is_inside_human = False
-
-
+        self.state = "I" if is_infected else "S"
--- environment.py
+++ environment.py
@@ -1,5 +1,5 @@
 import numpy as np
-from agents import Human, Virus
+from agents import Human
 from replay_buffer import ReplayBuffer
 class Environment:
     def __init__(self, grid_size: int, n_sick_humans: int, n_healthy_humans: int, n_viruses: int):
@@ -10,14 +10,10 @@ class Environment:
 
         self.replay_buffer = ReplayBuffer(max_size=10000, batch_size=32)
         self.humans = []
-        self.viruses = []
 
         for _ in range(n_sick_humans):
             self.humans.append(Human(np.random.randint(-grid_size, grid_size), np.random.randint(-grid_size, grid_size), is_infected=True))
         
         for _ in range(n_healthy_humans):
             self.humans.append(Human(np.random.randint(-grid_size, grid_size), np.random.randint(-grid_size, grid_size)))
-        
-        for _ in range(n_viruses):
-            self.viruses.append(Virus(np.random.randint(-grid_size, grid_size), np.random.randint(-grid_size, grid_size)))
 
